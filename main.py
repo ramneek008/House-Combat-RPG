84a7c88ac53d4a684875cba56954331d1a176bf8
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-05-06 17:36:45 +0530

    made rooms

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from room import Room
 
 kitchen = Room("Kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies.")
+kitchen.describe()
+
+dining_hall = Room("Dining Hall")
+dining_hall.set_description("")
+dining_hall.describe()
+
+ballroom = Room("Ballroom")
+ballroom.set_description("")
--- room.py
+++ room.py
@@ -2,3 +2,19 @@ class Room():
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+        self.linked_rooms = {}
+
+    def set_description(self, room_description):
+        self.description = room_description
+
+    def get_description(self):
+        return self.description
+
+    def get_name(self):
+        return self.name
+
+    def describe(self):
+        print(self.description)
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
