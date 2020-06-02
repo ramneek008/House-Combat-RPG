167c1d47fd2f95662df5cf6663800e947214860c
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-02 20:04:39 +0530

    conversation with monster

--- character.py
+++ character.py
@@ -11,3 +11,14 @@ class Character():
     def describe(self):
         print( self.name + " is here!" )
         print( self.description )
+
+    # Set what character will say
+    def set_conversation(self, conversation):
+        self.conversation = conversation;
+
+    # Talk to this character
+    def talk(self):
+        if self.conversation is not None:
+            print("[" + self.name + "]:" + self.conversation)
+        else:
+            print(self.name + "doesn't wants to talk") 
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from character import Character
 dave = Character("Dave", "A smelly zombie")
 dave.describe()
 
+dave.set_conversation("What's up, dude!")
+dave.talk()
+
 kitchen = Room("Kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies.")
 
