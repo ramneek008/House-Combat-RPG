6d633d17f2bf24b21e7475ebd8c206735db2467c
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-02 12:35:02 +0530

    added character component

---
+++ character.py
@@ -0,0 +1,13 @@
+
+class Character():
+
+    # Create a character
+    def __init__(self, char_name, char_description):
+        self.name = char_name
+        self.description = char_description
+        self.conversation = None
+
+    # Describe this character
+    def describe(self):
+        print( self.name + " is here!" )
+        print( self.description )
